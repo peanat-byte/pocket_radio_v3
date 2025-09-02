3d607f5ec7fdfefe00cfc0e3d2ae0d80596bce50
peanat-byte <amyinventions@gmail.com>
2025-09-01 21:29:41 -0700

    Basic working code!

--- fw/pocket_radio_v3/Core/Inc/main.h
+++ fw/pocket_radio_v3/Core/Inc/main.h
@@ -57,6 +57,30 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define VOL_P_Pin GPIO_PIN_0
+#define VOL_P_GPIO_Port GPIOB
+#define CHAN_M_Pin GPIO_PIN_1
+#define CHAN_M_GPIO_Port GPIOB
+#define CHAN_P_Pin GPIO_PIN_2
+#define CHAN_P_GPIO_Port GPIOB
+#define DIG1_Pin GPIO_PIN_11
+#define DIG1_GPIO_Port GPIOB
+#define DIG2_Pin GPIO_PIN_12
+#define DIG2_GPIO_Port GPIOB
+#define DIG4_Pin GPIO_PIN_13
+#define DIG4_GPIO_Port GPIOB
+#define DIG8_Pin GPIO_PIN_14
+#define DIG8_GPIO_Port GPIOB
+#define IND_Pin GPIO_PIN_15
+#define IND_GPIO_Port GPIOB
+#define VOL_M_Pin GPIO_PIN_3
+#define VOL_M_GPIO_Port GPIOB
+#define ADDR_SEL_Pin GPIO_PIN_5
+#define ADDR_SEL_GPIO_Port GPIOB
+#define GPO1_Pin GPIO_PIN_8
+#define GPO1_GPIO_Port GPIOB
+#define GPO2_Pin GPIO_PIN_9
+#define GPO2_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
--- fw/pocket_radio_v3/Core/Src/main.c
+++ fw/pocket_radio_v3/Core/Src/main.c
@@ -18,7 +18,8 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+#include <stdio.h>
+#include <string.h>
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -31,7 +32,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define ADDR 0x22
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -40,6 +41,9 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 
@@ -47,8 +51,22 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
+static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
-
+void Print_UART(char *str);
+void Power_Up(void);
+void Get_Rev(void);
+void Set_Property(void/*uint8_t property, uint8_t value*/);
+void Get_Property(void);
+void FM_Tune_Freq(uint16_t channel);
+void FM_Tune_Status(void);
+void FM_Seek_start(uint8_t direction);
+void GPIO_Ctl(void);
+void GPIO_Set(void);
+uint8_t Read_Status(void);
+void Wait_For_CTS(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -84,6 +102,9 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -92,6 +113,27 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
+
+	  HAL_GPIO_WritePin(GPIOB, DIG2_Pin, GPIO_PIN_SET);
+
+	  HAL_Delay(1000);
+
+	  char msg[64];
+	  sprintf(msg, "I'M ALIVEEEE");
+	  Print_UART(msg);
+
+	  Power_Up();
+	  Get_Rev();
+	  Set_Property();
+	  FM_Tune_Freq(10350);
+	  FM_Tune_Status();
+
+	  while (1)
+	  {
+		  HAL_Delay(10000);
+	  }
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -115,11 +157,14 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV1;
+  RCC_OscInitStruct.PLL.PLLN = 12;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -129,17 +174,318 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
   {
     Error_Handler();
   }
 }
 
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x10805D88;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, DIG1_Pin|DIG2_Pin|DIG4_Pin|DIG8_Pin
+                          |IND_Pin|ADDR_SEL_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : VOL_P_Pin CHAN_M_Pin CHAN_P_Pin VOL_M_Pin */
+  GPIO_InitStruct.Pin = VOL_P_Pin|CHAN_M_Pin|CHAN_P_Pin|VOL_M_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : DIG1_Pin DIG2_Pin DIG4_Pin DIG8_Pin
+                           IND_Pin */
+  GPIO_InitStruct.Pin = DIG1_Pin|DIG2_Pin|DIG4_Pin|DIG8_Pin
+                          |IND_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : ADDR_SEL_Pin */
+  GPIO_InitStruct.Pin = ADDR_SEL_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(ADDR_SEL_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : GPO1_Pin GPO2_Pin */
+  GPIO_InitStruct.Pin = GPO1_Pin|GPO2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
 /* USER CODE BEGIN 4 */
+void Print_UART(char *str)
+{
+  HAL_UART_Transmit(&huart2, (uint8_t*)str, strlen(str), HAL_MAX_DELAY);
+}
+
+/*
+* CMD 0x01 (POWER_UP)
+* ARG1 0x10
+* 	CTSIEN 0 (disable interrupt)
+* 	GPO2OEN 0 (disable GPO2) TODO: apparently Hi-Z draws current due to oscillations
+* 	PATCH 0 (boot normally)
+* 	XOSCEN 1 (use crystal oscillator)
+* 	FUNC 0 (FM receive)
+* ARG2 0x05 (00000101 analog audio output)
+*/
+void Power_Up(void)
+{
+  uint8_t cmd[3] = {0x01, 0x10, 0x05};  // CMD, ARG1, ARG2
+  HAL_I2C_Master_Transmit(&hi2c1, ADDR, cmd, sizeof(cmd), HAL_MAX_DELAY);
+  HAL_Delay(100);  // wait for CTS to go high hopefully
+}
+
+/*
+* CMD 0x10 (GET_REV)
+* Response: STATUS + 15 bytes
+*/
+void Get_Rev(void)
+{
+  uint8_t cmd = 0x10;
+  uint8_t resp[16] = {0};
+
+  HAL_I2C_Master_Transmit(&hi2c1, ADDR, &cmd, 1, HAL_MAX_DELAY);
+
+  HAL_Delay(100); // Hopefully CTS set by now
+
+  HAL_I2C_Master_Receive(&hi2c1, ADDR, resp, 16, HAL_MAX_DELAY);
+
+  char msg[64];
+  sprintf(msg, "STATUS: 0x%02X\r\n", resp[0]);
+  Print_UART(msg);
+  sprintf(msg, "Part Number: %d\r\n", resp[1]);
+  Print_UART(msg);
+  sprintf(msg, "FW Major: %d\r\n", resp[2]);
+  Print_UART(msg);
+  sprintf(msg, "FW Minor: %d\r\n", resp[3]);
+  int patchID = (resp[4] << 8) | resp[5];
+  sprintf(msg, "Patch ID: %d\r\n", patchID);
+  Print_UART(msg);
+  sprintf(msg, "Comp Major: %d\r\n", resp[6]);
+  Print_UART(msg);
+  sprintf(msg, "Comp Minor: %d\r\n", resp[7]);
+  Print_UART(msg);
+  sprintf(msg, "Chip Rev: %d\r\n", resp[8]);
+  Print_UART(msg);
+}
+
+/*
+* CMD 0x12 (SET_PROPERTY)
+* ARG1 0x00
+* ARG2 (property high byte) 0x11
+* ARG3 (property low byte) 0x07
+* ARG4 (value high byte) 0x00
+* ARG5(value low byte) 0x01 (LPI antenna input)
+*
+* TODO: currently just sets antenna to LPI lol
+* Properties:
+* 	FM_ANTENNA_INPUT 0x1107
+* 	RX_VOLUME 0x4000
+*/
+void Set_Property(void/*uint8_t property, uint8_t value*/)
+{
+  uint8_t cmd[6] = {0x12, 0x00, 0x11, 0x07, 0x00, 0x01};
+  HAL_I2C_Master_Transmit(&hi2c1, ADDR, cmd, sizeof(cmd), HAL_MAX_DELAY);
+  HAL_Delay(100);
+}
+
+void Get_Property(void)
+{
+  // TODO
+}
+
+/*
+* CMD 0x20 (FM_TUNE_FREQ)
+* ARG1 0x00
+* 	FREEZE 0 (bit 1)
+* 	FAST 0 (bit 0)
+* ARG2 (channel high byte)
+* ARG3 (channel low byte)
+* ARG4 0x00 (set antenna tuning cap automatically)
+*
+* @param channel in 10kHz units from 64-108MHz
+*/
+void FM_Tune_Freq(uint16_t channel)
+{
+  uint8_t FREQ_H = (channel >> 8) & 0xFF;
+  uint8_t FREQ_L = channel & 0xFF;
+  uint8_t cmd[5] = {0x20, 0x00, FREQ_H, FREQ_L, 0x00};
+  HAL_I2C_Master_Transmit(&hi2c1, ADDR, cmd, sizeof(cmd), HAL_MAX_DELAY);
+  HAL_Delay(100);
+}
+
+/*
+* CMD 0x22
+* ARG1 0x00
+* 	CANCEL 0 (don't cancel seek) (bit 1)
+* 	INTACK 0 (don't clear seek/tune interrupt status indicator) (bit 0)
+*/
+void FM_Tune_Status(void)
+{
+  uint8_t cmd[2] = {0x22, 0x00};
+  HAL_I2C_Master_Transmit(&hi2c1, ADDR, cmd, sizeof(cmd), HAL_MAX_DELAY);
+  HAL_Delay(100);
+
+  uint8_t resp[8] = {0};
+  HAL_I2C_Master_Receive(&hi2c1, ADDR, resp, 8, HAL_MAX_DELAY);
+
+  char msg[64];
+  sprintf(msg, "RESP1: 0x%02X\r\n", resp[1]);
+  Print_UART(msg);
+  int freq = (resp[2] << 8) | resp[3];
+  sprintf(msg, "Read frequency: %d\r\n", freq);
+  Print_UART(msg);
+  sprintf(msg, "RSSI: %d\r\n", resp[4]);
+  Print_UART(msg);
+  sprintf(msg, "SNR: %d\r\n", resp[5]);
+  Print_UART(msg);
+  sprintf(msg, "READANTCAP: %d\r\n", resp[7]);
+  Print_UART(msg);
+}
+
+void FM_Seek_start(uint8_t direction)
+{
+  // TODO
+}
+
+void GPIO_Ctl(void)
+{
+  // TODO
+}
+
+void GPIO_Set(void)
+{
+  //TODO
+}
+
+uint8_t Read_Status(void)
+{
+  uint8_t status = 0;
+  HAL_I2C_Master_Receive(&hi2c1, ADDR, &status, 1, HAL_MAX_DELAY);
+  return status;
+}
+
+void Wait_For_CTS(void)
+{
+  uint8_t status;
+  do {
+	  status = Read_Status();
+	  HAL_Delay(100);
+  } while ((status & 0x80) == 0); // While CTS bit low
+
+  // Print status
+  char msg[32];
+  sprintf(msg, "CTS high, STATUS = 0x%02X\r\n", status);
+  Print_UART(msg);
+}
 
 /* USER CODE END 4 */
 
